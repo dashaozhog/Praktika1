18d41feeec99364e0c626ab293084d6f7383b599
dashaozhog <126893190+dashaozhog@users.noreply.github.com>
2025-10-29 08:49:51 +0200

    індивідуальне завдання

---
+++ Praktika_Ind.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main()
+{   const int size=20;
+    int correct = 0;
+    string quest[size];
+    string ans[size];
+    ifstream fin("source.txt", ios_base::in);
+    if (!fin.is_open()) {
+        cout << "couldn't open file";
+        return 1;
+    }
+    string line;
+    int ind = 0;
+    while (getline(fin, line)) {  
+        if (ind % 2 == 0) {
+            quest[ind / 2] = line;
+        }
+        else {
+            ans[ind / 2] = line;
+        }
+        ind++;
+    }
+    string answer;
+    for (int i = 0; i < size; i++) {
+        cout <<i+1<<". " << quest[i] << endl;
+        cin >> answer;
+        if (answer == ans[i]) {
+            cout << "Correct!"<<endl;
+            correct++;
+        }
+        else {
+            cout << "Wrong!"<<endl;
+        }
+    }
+    cout << "Score: " << ((float)correct / size) * 100 << "%";
+    fin.close();
+    return 0;
+}
+
